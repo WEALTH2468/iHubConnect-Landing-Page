5c96ab5f36963b4fa4eaf0c859ffa3459eee2b26
etinyene ededem <etinyeneededem07@gmail.com>
2025-09-25 11:22:23 +0100

    files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-const App = () => {
+import React from "react";
+import WhyDigitalze from "./ihubConnect_sections/WhyDigitalze";
+
+
+function App()  {
   return (
     <div>
-      <h1 className="text-amber-200 text-4xl font-bold">App</h1>
+      <h1 className="text-amber-900 text-4xl font-bold">App mes</h1>
+
+
+      <WhyDigitalze />
     </div>
+
+    
   );
 };
 
